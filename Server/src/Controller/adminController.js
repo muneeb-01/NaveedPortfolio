499d3d29a9646db63c62ec86c9df4cc71578529e
Muneeb <muneerahmed8556@gmail.com>
2024-10-04 15:44:27 +0500

    creating server

--- Server/src/Controller/adminController.js
+++ Server/src/Controller/adminController.js
@@ -1,5 +1,15 @@
 const AdminModel = require("../Models/Admin");
 const dbgr = require("debug")("development:Admin");
+const jwt = require("jsonwebtoken");
+const { compare } = require("bcrypt");
+require("dotenv").config();
+
+const maxAge = 1000 * 60 * 60 * 24 * 3;
+const createToken = (email, userId) => {
+  return jwt.sign({ email, userId }, process.env.JWT_KEY, {
+    expiresIn: maxAge,
+  });
+};
 
 module.exports.CreateAdmin = async (req, res) => {
   try {
@@ -17,6 +27,35 @@ module.exports.CreateAdmin = async (req, res) => {
     res.send({ admin });
   } catch (error) {
     console.log(error);
-    dbgr("Error from (MODELS->AdminRoute) /create/admin");
+    dbgr("Error from (MODELS->AdminRoute) /create");
   }
+};
+
+module.exports.LoginAdmin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await AdminModel.findOne({ email });
+    if (!user) return res.status(200).send("Invalid email or password.");
+
+    const auth = await compare(password, user.password);
+    if (!auth) return res.status(200).send("Invalid email or password.");
+
+    res.cookie(
+      "jwt",
+      createToken(user.email, user._id, {
+        secure: true,
+        sameSite: "none",
+      })
+    );
+
+    res.send({ user });
+  } catch (error) {
+    console.log(error);
+    dbgr("Error from (MODELS->AdminRoute) /login");
+  }
+};
+
+module.exports.getAdminInfo = async () => {
+  try {
+  } catch (error) {}
 };
--- Server/src/Routes/AdminRoute.js
+++ Server/src/Routes/AdminRoute.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const AdminRoute = express.Router();
-const { CreateAdmin } = require("../Controller/adminController");
+const {
+  CreateAdmin,
+  LoginAdmin,
+  getAdminInfo,
+} = require("../Controller/adminController");
 
 if (process.env.NODE_ENV === "development") {
   AdminRoute.post("/create", CreateAdmin);
 }
+
+AdminRoute.post("/login", LoginAdmin);
+AdminRoute.post("/adminInfo", getAdminInfo);
 
 module.exports = AdminRoute;
